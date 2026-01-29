9473ba0e3660fb0f9d219b21c378d6c1f026983a
parameswara <parameswararaot@gmail.com>
2026-01-29 19:17:50 +0530

    Initial code

---
+++ FnCS_Casestudy2/fcs-interview-code-assignment-main/java-assignment/src/test/java/com/fulfilment/application/monolith/location/LocationGatewayTest.java
@@ -0,0 +1,38 @@
+package com.fulfilment.application.monolith.location;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import com.fulfilment.application.monolith.warehouses.domain.models.Location;
+import org.junit.jupiter.api.Test;
+
+public class LocationGatewayTest {
+
+    @Test
+    public void testWhenResolveExistingLocationShouldReturn() {
+        // given
+        LocationGateway locationGateway = new LocationGateway();
+
+        // when
+        Location location = locationGateway.resolveByIdentifier("ZWOLLE-001");
+
+        // then
+        assertNotNull(location);
+        assertEquals("ZWOLLE-001", location.identification);
+        assertEquals(1, location.maxNumberOfWarehouses);
+        assertEquals(40, location.maxCapacity);
+    }
+
+    @Test
+    public void testWhenResolveNonExistingLocationShouldReturnNull() {
+        // given
+        LocationGateway locationGateway = new LocationGateway();
+
+        // when
+        Location location = locationGateway.resolveByIdentifier("NON-EXISTENT");
+
+        // then
+        assertNull(location);
+    }
+}
